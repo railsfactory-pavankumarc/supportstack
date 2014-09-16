ff0f67f5ca7a0fbf6761f476d60aa5f52b371a77
Pavan <pavankumar@railsfactory.org>
2014-09-16 20:12:18 +0530

    commiting changes in config

--- supportstack/config/routes.rb
+++ supportstack/config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   resources :departments
   resources :users
   resources :tickets
+  resources :staff
+  resources :client
+
+  root 'users#index'
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
