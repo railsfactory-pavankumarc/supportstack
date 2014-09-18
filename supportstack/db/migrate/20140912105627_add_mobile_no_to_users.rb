1b86381693c5b6757c1d5ddd2d38cf2b66832670
Pavan <pavankumar@railsfactory.org>
2014-09-18 19:31:19 +0530

    removed unwanted files

--- supportstack/db/migrate/20140912105627_add_mobile_no_to_users.rb
+++
@@ -1,7 +0,0 @@
-class AddMobileNoToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :mobile_no, :string
-    add_column :users, :fax, :string
-    add_column :users, :active, :boolean, :default => true
-  end
-end
