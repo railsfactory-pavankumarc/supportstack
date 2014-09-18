5b5352a6040e33acdde138080520ffa41ab4b2b1
Pavan <pavankumar@railsfactory.org>
2014-09-18 19:34:47 +0530

    Folder structure modified

--- supportstack/app/models/comment.rb
+++
@@ -1,5 +0,0 @@
-class Comment < ActiveRecord::Base
-  has_many :attachments, as: :attachable
-  belongs_to :user_id
-  belongs_to :ticket_id
-end
--- supportstack/db/migrate/20140912110605_add_role_id_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddRoleIdToUsers < ActiveRecord::Migration
-  def change
-    add_reference :users, :role, index: true, :default => 'client' 
-    add_reference :users, :department, index: true
-  end
-end
