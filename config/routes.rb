6cb0c79eed00799ad0384a486a78f4e1ed4403f6
Pavan <pavankumar@railsfactory.org>
2014-09-23 19:32:38 +0530

    news controller created and views written for news

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   resources :client
   resources :comments
   root 'home#index'
-
+  get 'news/index'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
