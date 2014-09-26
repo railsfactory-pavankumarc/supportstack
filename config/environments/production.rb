13de177335aa6e7f565e504257c1e7785d92fa2b
Pavan <pavankumar@railsfactory.org>
2014-09-26 17:42:59 +0530

    config modified for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,8 +79,8 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.assets.precompile += %w[active_admin.css active_admin.js]
-  config.assets.initialize_on_precompile = false
-  
+  config.assets.initialize_on_precompile = true
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.smtp_settings = {
