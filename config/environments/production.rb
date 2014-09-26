866af4243c767c9a06572804035ad5913190a7ba
Pavan <pavankumar@railsfactory.org>
2014-09-26 15:22:11 +0530

    deploying into heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.smtp_settings = {
+address: "smtp.gmail.com",
+port: 587,
+domain: 'supportsatck.herokuapp.com' ,
+authentication: "plain",
+enable_starttls_auto: true,
+user_name: 'pavankumar@railsfactory.org',
+password: '23pavkumcha1992'
+}
 end
